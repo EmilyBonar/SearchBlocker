4fb6c7be04a8dad7fac951cd0ff7d5ce3906abef
emilybonar <emilykbonar@gmail.com>
2020-11-25 10:57:54 -0600

    Improved script

--- searchblock.js
+++ searchblock.js
@@ -1,3 +1,16 @@
-const results = document.querySelectorAll(".rc");
-console.log(browser.storage.local.get("domains"));
-//results[1].style.display = "none";
+const searchResults = document.querySelectorAll(".rc");
+console.log("test");
+getDomains().then((domains) => {
+	console.log("test3");
+	domains.forEach((domain) => {
+		console.log(domain);
+	});
+});
+
+browser.storage.onChanged.addListener((changes, areaName) => {
+	console.log("test4");
+});
+
+async function getDomains() {
+	return await browser.storage.local.get("domains");
+}

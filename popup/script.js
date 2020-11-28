8c33c8039ca422df017883e680408c1ba0603b45
emilybonar <emilykbonar@gmail.com>
2020-11-28 11:45:34 -0600

    Implemented clear button

--- popup/script.js
+++ popup/script.js
@@ -1,12 +1,14 @@
-const buttons = document.querySelectorAll(".button");
-const del = buttons[1];
-const add = buttons[0];
+const del = document.querySelector(".delete");
+const add = document.querySelector(".add");
+const clear = document.querySelector(".clear");
 
 const selector = document.querySelector("select");
 
 del.addEventListener("click", (e) => {
 	console.log(selector.selectedOptions[0].text);
-	browser.storage.local.get("domains");
+	browser.storage.local.get("domains").then((domains) => {
+		deleteOption(selector.selectedOptions[0].text);
+	});
 });
 
 add.addEventListener("click", (e) => {
@@ -25,8 +27,29 @@ add.addEventListener("click", (e) => {
 	addOption(domainValue);
 });
 
+clear.addEventListener("click", (e) => clearOptions());
+
 function addOption(domain) {
 	let option = document.createElement("option");
 	option.text = domain;
 	selector.add(option);
+}
+
+function deleteOption(domain) {
+	let domains = [...selector.options];
+	console.log(selector.options.length);
+	domains.forEach((item) => console.log(item.text));
+}
+
+function syncOptions() {}
+
+function clearOptions() {
+	browser.storage.local.set({
+		domains: [],
+	});
+	[...selector.options].forEach((item, index) => {
+		if (index != 0) {
+			selector.remove(index);
+		}
+	});
 }
